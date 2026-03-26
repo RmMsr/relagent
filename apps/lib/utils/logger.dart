c9b99912fe5bd419549f7e7c6e3acc891088fe26
Roman <lynx@i-comp.de>
2026-03-26 12:19:52 +0100

    feat: suppress info and debug logs during test runs by default

--- apps/lib/utils/logger.dart
+++ apps/lib/utils/logger.dart
@@ -5,9 +5,14 @@ enum LogLevel { debug, info, warning, error }
 
 /// Minimalistic logger with configurable levels
 /// Debug builds log at debug level; release builds log at info level and above
+/// Test runs default to warning level (info and debug suppressed) unless verbose logging enabled
 class Logger {
-  static final LogLevel _minLevel =
-      kReleaseMode ? LogLevel.info : LogLevel.debug;
+  static final LogLevel _minLevel = kReleaseMode
+      ? LogLevel.info
+      : const bool.fromEnvironment('debug_logs_enabled', defaultValue: false) ==
+            false
+      ? LogLevel.warning
+      : LogLevel.debug;
 
   static void debug(String message) {
     _log(LogLevel.debug, message);
